3a654b637338cfbcee0c7b451972a80d9f177acf
Алина <ap9321189@gmail.com>
2026-05-12 10:09:44 +0300

    Добавлен класс Transaction

---
+++ butgem/Transaction.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace butgem
+{
+    public enum TransactionType
+    {
+        Доход,
+        Расход
+    }
+
+    public class Transaction
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction(string description, decimal amount, TransactionType type, DateTime date)
+        {
+            Description = description;
+            Amount = amount;
+            Type = type;
+            Date = date;
+        }
+    }
+}
